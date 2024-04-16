3ff1640f37092ca08f4787762302b97e46190eec
Suvid Singhal <anooptest100@gmail.com>
2024-04-16 15:28:36 +0530

    feat: Step 20

--- tests/step-05/index.test.js
+++ tests/step-05/index.test.js
@@ -1,25 +1,30 @@
-const readCSV = require('../../src/csvReader');
-const {parseQuery} = require('../../src/queryParser');
-const executeSELECTQuery = require('../../src/index');
+const { readCSV } = require('../../src/csvReader');
+const { parseSelectQuery } = require('../../src/queryParser');
+const { executeSELECTQuery } = require('../../src/index');
 
 test('Read CSV File', async () => {
     const data = await readCSV('./student.csv');
     expect(data.length).toBeGreaterThan(0);
-    expect(data.length).toBe(4);
+    expect(data.length).toBe(5);
     expect(data[0].name).toBe('John');
     expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
 });
 
 test('Parse SQL Query', () => {
     const query = 'SELECT id, name FROM student';
-    const parsed = parseQuery(query);
+    const parsed = parseSelectQuery(query);
     expect(parsed).toEqual({
         fields: ['id', 'name'],
         table: 'student',
-        whereClauses: [],
-        joinType: null,
         joinCondition: null,
-        joinTable: null
+        joinType: null,
+        joinTable: null,
+        whereClauses: [],
+        groupByFields: null,
+        hasAggregateWithoutGroupBy: false,
+        "orderByFields": null,
+        "limit": null,
+        "isDistinct": false// Update to match the new structure
     });
 });
 
@@ -35,18 +40,25 @@ test('Execute SQL Query', async () => {
 
 test('Parse SQL Query with WHERE Clause', () => {
     const query = 'SELECT id, name FROM student WHERE age = 25';
-    const parsed = parseQuery(query);
+    const parsed = parseSelectQuery(query);
     expect(parsed).toEqual({
         fields: ['id', 'name'],
         table: 'student',
-        whereClauses: [{
-            "field": "age",
-            "operator": "=",
-            "value": "25",
-        }],
-        joinType: null,
         joinCondition: null,
-        joinTable: null
+        joinType: null,
+        joinTable: null,
+        whereClauses: [
+            {
+                field: 'age',
+                operator: '=',
+                value: '25'
+            }
+        ],
+        groupByFields: null,
+        hasAggregateWithoutGroupBy: false,
+        "orderByFields": null,
+        "limit": null,
+        "isDistinct": false
     });
 });
 
